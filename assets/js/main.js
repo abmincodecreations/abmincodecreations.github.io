8b04a62a5d2fcc57187b2a53c04de95e7020605a
abmincodecreations <abmabhishekmishra@gmail.com>
2021-11-17 15:12:46 +0530

    UX page design and nav update

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,40 +130,8 @@
     }
   });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-  });
-
-  // Initiate venobox (lightbox feature used in portofilo)
-  $(document).ready(function() {
-    $('.venobox').venobox({
-      'share': false
-    });
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
 })(jQuery);
+
 // services section (animee)
 var anyclickonpage= false;
 function animee() {
@@ -237,8 +205,7 @@ function WindowSizeAdjustor(){
 }
 
 window.addEventListener("resize", WindowSizeAdjustor);
-  
-
+ 
 window.onload = function pageReloadUiManager(){
   
   var currentUrl="hii"+window.location.href;
