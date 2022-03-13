a63f2d4ffec2e70588432301ee3705088f49ed05
abmincodecreations <abmabhishekmishra@gmail.com>
2022-03-13 13:29:45 +0530

    loding forever set up

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,6 +219,7 @@ function SetLodingPageModeOff(){
 window.addEventListener("resize", WindowSizeAdjustor);
  
 window.onload = function pageReloadUiManager(){
+  setTimeout(SetLodingPageModeOff, 7000);
   SetLodingPageModeOff();
   document.getElementsByTagName('h1')[0].focus();
   var currentUrl="hii"+window.location.href;
